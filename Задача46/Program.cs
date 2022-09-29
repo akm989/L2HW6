d555ec9837fd92c73ffed0d5df9522ef28ed3633
Akmal <akmal_179a@mail.ru>
2022-09-29 21:30:52 +0500

    46 решение

---
+++ Задача46/Program.cs
@@ -0,0 +1,21 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+Console.Beep();
+Console.Write("Введите числа: ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), Convert.ToInt32);
+int count = 0;
+ 
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+ 
+Console.WriteLine($"Кол-во элементов выше 0 - > : {count}");
